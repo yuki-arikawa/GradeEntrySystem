5fd5c879538077a0bfaafd1b91fc37226a571342
yarikawa <yarikawa0220@gmail.com>
2024-11-27 13:46:52 +0900

    next.configに設定追加

--- grade-entry-system/frontend/next.config.ts
+++ grade-entry-system/frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Webpack 設定をカスタマイズ
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false, // async_hooks を無視
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
