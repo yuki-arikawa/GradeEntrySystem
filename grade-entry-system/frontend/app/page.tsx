c52b0ff2f2bac9ef3f0658e305c43843c1c59b06
yarikawa <yarikawa0220@gmail.com>
2024-12-04 12:01:26 +0900

    教員用ページの雛形作成

--- grade-entry-system/frontend/app/page.tsx
+++ grade-entry-system/frontend/app/page.tsx
@@ -1,18 +1,7 @@
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const runtime = 'edge';
 
 export default async function Home() {
-  // クッキーからトークン取得
-  const cookieStore = await cookies();
-  const token = cookieStore.get('token')?.value;
-
-  if(token){
-    redirect('/dashboard');
-  }else{
-    redirect('/login');
-  }
-
-  return null;
+  redirect('/login');
 }
